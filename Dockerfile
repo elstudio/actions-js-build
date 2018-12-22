2bd7a0afb0ee2688ceaf2e4f889dbe0dd502db21
Eric Johnson <elstudio@github.com>
2018-12-22 13:18:49 -0500

    Hardcode grunt exec for a quick test.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM node:10-slim
+FROM node:lts-alpine
 
 LABEL version="1.0.0"
-LABEL repository="http://github.com/actions/npm"
-LABEL homepage="http://github.com/actions/npm"
+LABEL repository="https://github.com/elstudio/actions-js-build"
+LABEL homepage="https://github.com/elstudio/actions-js-build"
 LABEL maintainer="GitHub Actions <support+actions@github.com>"
 
-LABEL com.github.actions.name="GitHub Action for npm"
-LABEL com.github.actions.description="Wraps the npm CLI to enable common npm commands."
+LABEL com.github.actions.name="GitHub Action for javascript build tools"
+LABEL com.github.actions.description="Wraps the npm CLI to enable common js build commands."
 LABEL com.github.actions.icon="package"
 LABEL com.github.actions.color="red"
 COPY LICENSE README.md THIRD_PARTY_NOTICE.md /
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-if [ -n "$NPM_AUTH_TOKEN" ]; then
-  # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
-  NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+# if [ -n "$NPM_AUTH_TOKEN" ]; then
+#   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
+#   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
+#   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
 
-  # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//$NPM_REGISTRY_URL/:_authToken=$NPM_AUTH_TOKEN\nregistry=$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
-  chmod 0600 "$NPM_CONFIG_USERCONFIG"
-fi
+#   # Allow registry.npmjs.org to be overridden with an environment variable
+#   printf "//$NPM_REGISTRY_URL/:_authToken=$NPM_AUTH_TOKEN\nregistry=$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+#   chmod 0600 "$NPM_CONFIG_USERCONFIG"
+# fi
 
-sh -c "npm $*"
+cd $WD_PATH
+npm install
+grunt
+git status
+# sh -c "npm $*"
