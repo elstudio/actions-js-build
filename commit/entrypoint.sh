93a34950e40442f77432b3414d15ff66e9bf75b5
Eric Johnson <elstudio@users.noreply.github.com>
2018-12-26 09:08:56 -0500

    Remove extra logging.

--- commit/entrypoint.sh
+++ commit/entrypoint.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-#echo "Installing git"
-#apk update
-#apk add git
-
-pwd
 if [ -n "$WD_PATH" ]
 then
   echo "Changing dir to $WD_PATH"
   cd $WD_PATH
 fi
-
-git status
 
 # This section only runs if there have been file changes
 if ! git diff --quiet
@@ -21,4 +14,6 @@ then
   git add .
   git commit -m "Regenerate build artifacts."
   git push
+else 
+  echo "Working tree clean. Nothing to commit."
 fi
