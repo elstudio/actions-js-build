d76f931813ff74a3ab5b214e9508ea7a46ffdb0c
Eric Johnson <elstudio@users.noreply.github.com>
2018-12-26 12:34:13 -0500

    Fix gruntfile conditional.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,15 +8,15 @@ npm install
 
 # First try Gulp, then try Grunt
 if [ -f "gulpfile.js" ]
-  then 
-    npm install -g gulp-cli
-    echo "Running Gulp default task"
-    gulp
-elif [ -f "Gruntfile.js " ]
-  then 
-    npm install -g grunt-cli
-    echo "Running Grunt default task"
-    grunt
+then 
+  npm install -g gulp-cli
+  echo "Running Gulp default task"
+  gulp
+elif [ -f "Gruntfile.js" ]
+then 
+  npm install -g grunt-cli
+  echo "Running Grunt default task"
+  grunt
 else
   echo "Neither Gruntfile nor gulpfile found. Nothing to execute."
 fi
