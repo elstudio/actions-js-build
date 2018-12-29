b936bf69429fdc181dec3aa8ff539b5432d26f36
Eric Johnson <elstudio@users.noreply.github.com>
2018-12-29 08:38:33 -0500

    Write github credentials to netrc file.

--- commit/entrypoint.sh
+++ commit/entrypoint.sh
@@ -8,12 +8,31 @@ then
   cd $WD_PATH
 fi
 
+# Set up .netrc file
+setup_netrc ( ) {
+  cat << EOF > $HOME/.netrc
+machine github.com
+login $GITHUB_ACTOR
+password $GITHUB_TOKEN
+
+machine api.github.com
+login $GITHUB_ACTOR
+password $GITHUB_TOKEN
+
+EOF
+  chmod 600 $HOME/.netrc
+  echo "HOME directory contents"
+  ls -al $HOME
+}
+
 # Show the event json
 cat $GITHUB_EVENT_PATH
+
 
 # This section only runs if there have been file changes
 if ! git diff --quiet
 then 
+  setup_netrc
   git config --global user.email "servers@el-studio.com"
   git config --global user.name "$GITHUB_ACTOR"
   git add .
