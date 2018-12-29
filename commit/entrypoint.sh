4a85c6bb944fff7654eb702211f2e9eae6e73409
Eric Johnson <elstudio@users.noreply.github.com>
2018-12-29 15:56:32 -0500

    Remove -e param from awk.

--- commit/entrypoint.sh
+++ commit/entrypoint.sh
@@ -35,8 +35,8 @@ then
   git_setup
   git add .
   git commit -m "Regenerate build artifacts."
-  GITHUB_BRANCH=`echo "$GITHUB_REF" | awk -F / -e '{ print $3 }'`
-  git push --set-upstream origin $GITHUB_BRANCH
+  branch=`echo "$GITHUB_REF" | awk -F / '{ print $3 }' `
+  git push --set-upstream origin $branch
 else 
   echo "Working tree clean. Nothing to commit."
 fi
