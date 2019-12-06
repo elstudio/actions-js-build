a41b9769831e377b656d7adeaec1de21acb52bb8
Eric Johnson <elstudio@github.com>
2019-12-05 20:56:12 -0500

    Remove manual setup of git credentials.

--- commit/entrypoint.sh
+++ commit/entrypoint.sh
@@ -30,16 +30,16 @@ fi
 
 # Set up .netrc file with GitHub credentials
 git_setup ( ) {
-  cat <<- EOF > "$HOME/.netrc"
-		machine github.com
-		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
+#   cat <<- EOF > "$HOME/.netrc"
+# 		machine github.com
+# 		login $GITHUB_ACTOR
+# 		password $GITHUB_TOKEN
 
-		machine api.github.com
-		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
-EOF
-  chmod 600 "$HOME/.netrc"
+# 		machine api.github.com
+# 		login $GITHUB_ACTOR
+# 		password $GITHUB_TOKEN
+# EOF
+#   chmod 600 "$HOME/.netrc"
 
   # Git requires our "name" and email address -- use GitHub handle
   git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
