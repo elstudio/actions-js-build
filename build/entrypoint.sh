d1f3aa76c21aeb495aa21ae49953436b2734ff7d
Colin Walsh <cwalsh003@users.noreply.github.com>
2020-06-11 12:40:38 -0400

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -21,20 +21,9 @@ echo $PWD
 echo "Installing NPM dependencies"
 npm install
 
-# First try Gulp, then try Grunt
-# Gulpfile.js can be a file or a directory:
-if [ -e "gulpfile.js" ]
-then 
-  npm install -g gulp-cli
-  echo "Running Gulp with args"
-  sh -c "gulp $*"
-# Gruntfile can be js or coffeescript file
-elif [ -f "Gruntfile.js" -o -f "Gruntfile.coffee" ]
-then 
-  npm install -g grunt-cli
-  echo "Running Grunt with args"
-  sh -c "grunt $*"
-else
-  echo "Running NPM with args"
-  sh -c "npm $*"
-fi
+
+
+npm install -g gulp-cli
+echo "Running Gulp with args"
+sh -c "gulp $*"
+
